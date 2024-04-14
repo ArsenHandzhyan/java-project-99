46b21c6dd88f3c50ec8f156baad963d51c872cf8
arsen <arsenhan@internet.ru>
2024-04-14 03:38:01 +0300

    add checkstyle.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,53 +2,58 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
-	java
-	id("org.springframework.boot") version "3.3.0-SNAPSHOT"
-	id("io.spring.dependency-management") version "1.1.4"
-	id("com.github.ben-manes.versions") version "0.49.0"
-	id("io.freefair.lombok") version "8.4"
-	id("jacoco")
+    java
+    id("org.springframework.boot") version "3.3.0-SNAPSHOT"
+    id("io.spring.dependency-management") version "1.1.4"
+    id("com.github.ben-manes.versions") version "0.49.0"
+    id("io.freefair.lombok") version "8.4"
+    id("jacoco")
+    checkstyle
 }
 
 group = "hexlet.code"
-version = "0.0.1-SNAPSHOT"
+version = "1.0-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_20
+    sourceCompatibility = JavaVersion.VERSION_20
 }
 
 repositories {
-	mavenCentral()
-	maven { url = uri("https://repo.spring.io/milestone") }
-	maven { url = uri("https://repo.spring.io/snapshot") }
+    mavenCentral()
+    maven { url = uri("https://repo.spring.io/milestone") }
+    maven { url = uri("https://repo.spring.io/snapshot") }
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-devtools")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-devtools")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
-	runtimeOnly("com.h2database:h2")
+    runtimeOnly("com.h2database:h2")
 
-	implementation("org.springframework.boot:spring-boot-starter")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
-	// Настройка логирования для тестов
-	testLogging {
-		exceptionFormat = TestExceptionFormat.FULL
-		events = setOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
-		// showStackTraces = true
-		// showCauses = true
-		showStandardStreams = true
-	}
+    useJUnitPlatform()
+    // Настройка логирования для тестов
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = setOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
 }
 
 tasks.named<JacocoReport>("jacocoTestReport") {
-	reports {
-		xml.required.set(true)
-	}
+    reports {
+        xml.required.set(true)
+    }
+}
+
+checkstyle {
+    toolVersion = "10.12.1"
 }
