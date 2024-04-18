bbdca882bc58c042edffc19f96ba207a1181c576
arsen <arsenhan@internet.ru>
2024-04-18 03:24:14 +0300

    add UserMapper

--- app/src/main/java/hexlet/code/app/mapper/UserMapper.java
+++ app/src/main/java/hexlet/code/app/mapper/UserMapper.java
@@ -5,7 +5,6 @@ import hexlet.code.app.dto.UserCreateDTO;
 import hexlet.code.app.dto.UserUpdateDTO;
 import hexlet.code.app.model.User;
 import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
 import org.openapitools.jackson.nullable.JsonNullable;
@@ -24,25 +23,5 @@ public abstract class UserMapper {
     // Добавленный метод преобразования
     public String map(JsonNullable<String> value) {
         return value != null ? value.orElse(null) : null;
-    }
-
-    public User update(UserUpdateDTO dto, @MappingTarget User model) {
-        if (dto.getEmail() != null) {
-            model.setEmail(map(dto.getEmail()));
-        }
-
-        if (dto.getFirstName() != null) {
-            model.setFirstName(map(dto.getFirstName()));
-        }
-
-        if (dto.getLastName() != null) {
-            model.setLastName(map(dto.getLastName()));
-        }
-
-        if (dto.getPassword() != null) {
-            model.setPassword(map(dto.getPassword()));
-        }
-
-        return model;
     }
 }
